f527d330d2685e7256b9d1040dd6d0582dfb6d92
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-06-03 20:45:13 -0400

    staircase

---
+++ staircase.js
@@ -0,0 +1,9 @@
+const staircase = n => {
+    const line = Array(n + 1).fill(' ');
+    //line[n] = ''; 
+    for (let i = n - 1; i >= 0; i--) {
+      line[i] = '#';
+      console.log(line.join(''));
+    }
+}
+staircase(7)
