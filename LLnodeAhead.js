982594238c707f234a4d89cf26331d47b024b581
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-09-23 13:12:29 -0400

    Linked list manipulation

---
+++ LLnodeAhead.js
@@ -0,0 +1,21 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtHead(head, data) {
+
+    const newNode = new SinglyLinkedListNode()
+    newNode.data = data
+    newNode.next = head
+
+    return newNode
+
+
+}
---
+++ llnodeTail.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+    }
+
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtTail(head, data) {
+    const newNode = new SinglyLinkedListNode();
+    newNode.data = data;
+    newNode.next = null;
+
+    if(head === null){
+        head = newNode;
+    }
+    else{
+        let current = head;
+        while(current.next !== null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    return head
+}
